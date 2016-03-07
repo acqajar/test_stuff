a9d2825fe803e5dc23989ab9f8edb31dec7cd6f5
Arsames Qajar <acqajar@gmail.com>
2016-03-06 17:51:22 -0800

    products showing on category pages

--- models/product.js
+++ models/product.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
+// var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
 
-var ProductSchema = new mongoose.Schema({
+var ProductSchema = new Schema({
   category: { type: Schema.Types.ObjectId, ref: 'Category'},
-	name: String,
+  name: String,
   price: Number,
   image: String
 });
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var Product = require('../models/product');
 
 
 router.get('/', function(req, res){
@@ -19,4 +20,42 @@ router.get('/users', function(req,res){
 		res.json(users);
 	 	console.log(users);
 	});
+});
+
+// double dot specifies certain route
+// router.get('/products/:id', function(req,res,next){
+// 	Product //querying a product
+// 		.find({ category: req.params.id}) //how to access parameters in the route :id
+// 		.populate('category') //only if category is ObjectId which is true in Category Schema
+// 		.exec(function(err,products){ //execute anonymous funciton on all methods i.e. .find and .exec
+// 			if(err) return next(err);
+// 			res.render('main/category', {
+// 				products: products
+// 			});
+// 		});
+// });
+
+
+
+
+router.get('/products/:id', function(req, res, next) {
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+});
+
+
+router.get('/product/:id', function(req, res, next) {
+  Product.findById({ _id: req.params.id }, function(err, product) {
+    if (err) return next(err);
+    res.render('main/product', {
+      product: product
+    });
+  });
 });
